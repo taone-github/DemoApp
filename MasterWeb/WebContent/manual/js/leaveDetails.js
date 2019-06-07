f6dadac62de9786d847272a1f90db13d98f5b24a
arthit-github <48174895+arthit-github@users.noreply.github.com>
2019-06-07 16:52:39 +0700

    Add files via upload
    Leave Detail

---
+++ MasterWeb/WebContent/manual/js/leaveDetails.js
@@ -0,0 +1,21 @@
+function checkdateFromTo(fromFieldName, toFieldName) {
+	var fromValue = $("[name='" + fromFieldName + "']").val();
+	var toValue = $("[name='" + toFieldName + "']").val();	
+	if((fromValue.length==10)&&(toValue.length==10)){
+		if ((fromValue != '') && (toValue != '')) {
+			var fromSprit = fromValue.split('/');
+			var toSprit = toValue.split('/');
+			var toInt = parseInt((toSprit[2] + toSprit[1] + toSprit[0]), 10);
+			var fromInt = parseInt((fromSprit[2] + fromSprit[1] + fromSprit[0]), 10);
+			if (fromInt > toInt) {
+				var toFieldLabel = $("div [id *= '"+toFieldName+"_LabelField']").text().replace('*',"")
+				var fromFieldLabel = $("div [id *= '"+fromFieldName+"_LabelField']").text().replace('*',"");
+				alert(toFieldLabel + " cannot be less than " + fromFieldLabel);
+				$("[name='"+ toFieldName + "']").val('');
+				return false;
+			}
+			return true;
+		}
+	}
+	return true;
+}
